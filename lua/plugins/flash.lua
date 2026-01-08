30852019ab965341765d4c4d1acf44308d0847a3
Joseph Rubinson Geo Leemson (XC-CP/ECC2.4) <GeoLeemson.JosephRubinson@us.bosch.com>
2026-01-08 10:20:23 -0500

    Changed flash keymap to 'fl' due to mini issues

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -3,7 +3,7 @@ return{
     "folke/flash.nvim",
     opts = {},
     keys = {
-      {"s", function() require("flash").jump() end, mode = {"n", "x", "o"}, desc = "Flash jump"}
+      {"fl", function() require("flash").jump() end, mode = {"n", "x", "o"}, desc = "Flash jump"}
     },
   },
 }
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -78,18 +78,18 @@ return {
             -- INFO:
             -- saiw surround with no whitespace
             -- saw surround with whitespace
-            mappings = {
-                add = 'sa',            -- Add surrounding in Normal and Visual modes
-                delete = 'ds',         -- Delete surrounding
-                find = 'sf',           -- Find surrounding (to the right)
-                find_left = 'sF',      -- Find surrounding (to the left)
-                highlight = 'sh',      -- Highlight surrounding
-                replace = 'sr',        -- Replace surrounding
-                update_n_lines = 'sn', -- Update `n_lines`
-
-                suffix_last = 'l',     -- Suffix to search with "prev" method
-                suffix_next = 'n',     -- Suffix to search with "next" method
-            },
+            -- mappings = {
+            --     add = 'sa',            -- Add surrounding in Normal and Visual modes
+            --     delete = 'ds',         -- Delete surrounding
+            --     find = 'sf',           -- Find surrounding (to the right)
+            --     find_left = 'sF',      -- Find surrounding (to the left)
+            --     highlight = 'sh',      -- Highlight surrounding
+            --     --replace = 'sr',        -- Replace surrounding
+            --     update_n_lines = 'sn', -- Update `n_lines`
+            --
+            --     suffix_last = 'l',     -- Suffix to search with "prev" method
+            --     suffix_next = 'n',     -- Suffix to search with "next" method
+            -- },
 
             -- Number of lines within which surrounding is searched
             n_lines = 20,
